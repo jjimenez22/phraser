7aa20fa98a262f48edb0258cb59aabbe2465bad6
Jesus Jimenez <jgjimenezb94@gmail.com>
2020-06-21 15:34:23 -0300

    phrases

--- phrase/models.py
+++ phrase/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Phrase(models.Model):
+    text = models.CharField(max_length=250)
+    publish_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
